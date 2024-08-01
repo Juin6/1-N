7299e36a5568dfdca6e5b67bc490efbcc4f01b3f
Taejoo Kim <tj98724@gmail.com>
2024-08-01 12:01:12 +0900

    read comment 기능 추가

--- articles/admin.py
+++ articles/admin.py
@@ -5,5 +5,3 @@ from .models import Article, Comment
 admin.site.register(Article)
 admin.site.register(Comment)
 
-
-
--- articles/models.py
+++ articles/models.py
@@ -5,7 +5,9 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    # comment_set 라는 컬럼도 foreignkey에 의해 자동으로 생성됨.
 
 class Comment(models.Model):
     content = models.TextField()
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    # article_id 라는 컬럼은 자동으로 생성해줌
