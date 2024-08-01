72effe94571f8d5dac295866e199f30e3e983056
Taejoo Kim <tj98724@gmail.com>
2024-08-01 10:13:54 +0900

    read(all) 기능구현

--- articles/urls.py
+++ articles/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
+
+app_name = 'articles'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('articles/', include('articles.urls'))
+    path('', views.index, name='views'),
 ]
